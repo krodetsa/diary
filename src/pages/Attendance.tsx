c306278168610eb8886a173b5f71957f53305b9f
krodetsa <krodetsa@gmail.com>
2020-01-28 20:10:42 +0300

    фикс календаря

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,withIonLifeCycle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonCard, IonToolbar,withIonLifeCycle, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/react';
 import Calendar from 'react-calendar';
 import '../theme/Main.css';
 import '../theme/attendance.css';
@@ -13,7 +13,7 @@ user_id: string,
 interface IMyComponentState {
   currentDate: any,
   store: any,
-  attendancePerDate: string
+  attendancePerDate: any
 };
 class Tab3Page extends React.Component<IMyComponentProps, IMyComponentState> {
   constructor(props: Readonly<IMyComponentProps>) {
@@ -21,12 +21,12 @@ class Tab3Page extends React.Component<IMyComponentProps, IMyComponentState> {
   this.state = {
    currentDate: new Date().valueOf(),
    store: [],
-   attendancePerDate: '',
+   attendancePerDate: [],
  }
 }
 ionViewDidEnter() {
 
-  jsonp( "https://m.log.school/web/clnd.php?"+this.props.user_id+"=ng_jsonp_callback_3", {  name: 'ng_jsonp_callback_3' }, (error, data) => {
+  jsonp( "https://m.log.school/web/clnd.php?"+this.props.user_id+"=ng_jsonp_callback_0", {  name: 'ng_jsonp_callback_0' }, (error, data) => {
       if (error) {
           console.log(error)
       } else {
@@ -39,14 +39,21 @@ ionViewDidEnter() {
 dateChanged = date => {
   console.log(date)
   this.setState({ currentDate: date.valueOf() });
-  console.log(date.getDate())
+  console.log(date.getDate());
+  var att = new Array;
   this.state.store.forEach(el => {
     var newDate = new Date(el.start);
-    if(new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate()).valueOf() === this.state.currentDate) {
-
-    console.log(el)
+    if(new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate()).valueOf() === date.valueOf()) {
+      att.push(el)
     }
   })
+  this.setState((state) => {
+    // Важно: используем `state` вместо `this.state` при обновлении.
+    return {attendancePerDate: att}
+  });
+
+  // this.setState({ attendancePerDate: att });
+  console.log(this.state.attendancePerDate)
 }
   render() {
     return (
@@ -57,7 +64,35 @@ dateChanged = date => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <Calendar onClickDay={this.dateChanged} view={'month'} value={new Date(this.state.currentDate)}/>
+          <Calendar onClickDay={e => { this.dateChanged(e)}} view={'month'} value={new Date(this.state.currentDate)}/>
+            <IonCard>
+            <IonList>
+          {
+            this.state.attendancePerDate.length > 0 ?
+            (this.state.attendancePerDate.map((el, i) => {
+              var text = el.text;
+              var date = new Date(el.start).getDate();
+              console.log(date)
+              console.log(text)
+              return (
+                <IonItem key={i}>
+                  <IonLabel slot="start">
+                    {date}
+                  </IonLabel>
+                  <IonLabel>
+                  {text.replace(/<\/?[^>]+>/g,'')}
+                  </IonLabel>
+                </IonItem>)
+            })) : (
+
+                <IonLabel>
+                Нет событий
+                </IonLabel>
+
+            )
+          }
+          </IonList>
+          </IonCard>
         </IonContent>
       </IonPage>
     );
